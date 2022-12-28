57888d72cb6e33cb0cb2ee34fae1df9435dac84d
omnia rafat <76200523+OmniaRafat232111121@users.noreply.github.com>
2022-12-28 16:12:14 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -2,3 +2,15 @@ const header = document.querySelector('header');
 window.addEventListener('scroll', function () {
     header.classList.toggle("sticky",window.scrollY > 0)
 })
+
+function toggleMenu() {
+    const menuToggle = document.querySelector('.menuToggle');
+    const navigation = document.querySelector('.navigation');
+    console.log('jdj');
+    menuToggle.classList.toggle('active');
+    console.log('djjdjggg')
+    navigation.classList.toggle('active');
+     console.log('djjdjggg')
+      
+   
+}
